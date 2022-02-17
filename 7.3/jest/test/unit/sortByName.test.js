8c125a083fe2fb81f8ab206c7f058006a1fdec81
mac <dmitri-shadri@yandex.ru>
2022-02-17 23:39:21 +0300

    first commit

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,29 @@
 const sorting = require("../../app");
 
-describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+describe("Books names test", () => {
+  it("1", () => {
+    let books = ["Властелин Колец", "Гарри Поттер", "Питер Пэн"];
+    let expected = ["Властелин Колец", "Гарри Поттер", "Питер Пэн"];
+    expect(sorting.sortByName(books)).toEqual(expected);
+  });
+  it("2", () => {
+    let books = [
       "Властелин Колец",
-      "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+      "Властелин Колец",
+      "Гарри Поттер",
+    ];
+    let expected = [
+      "Волшебник изумрудного города",
+      "Питер Пэн",
+      "Властелин Колец",
+      "Гарри Поттер",
+    ];
+    expect(sorting.sortByName(books)).not.toEqual(expected);
+  });
+  it("3", () => {
+    let books = ["UFC12"];
+    let expected = ["UFC2"];
+    expect(sorting.sortByName(books)).toEqual(expected);
   });
 });
