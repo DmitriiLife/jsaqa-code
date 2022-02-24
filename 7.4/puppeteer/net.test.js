3f6e9ed9767849a70bb6b98a6f6068c8ef6b0379
mac <dmitri-shadri@yandex.ru>
2022-02-24 16:13:42 +0300

    add timeout

--- 7.4/puppeteer/net.test.js
+++ 7.4/puppeteer/net.test.js
@@ -1,18 +1,17 @@
 let page;
 let time;
-beforeEach(async () => {
-  page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(time);
-  await page.goto("https://netology.ru");
-});
-
-afterEach(() => {
-  page.close();
-});
 
 describe("Netology.ru tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://netology.ru");
+  });
+
+  afterEach(() => {
+    page.close();
+  });
+
   test("The first test'", async () => {
-    time = 6000;
     const title = await page.title();
     console.log("Page title: " + title);
     const firstLink = await page.$("header a + a");
@@ -23,60 +22,66 @@ describe("Netology.ru tests", () => {
     const pageList = await browser.newPage();
     await pageList.goto("https://netology.ru/navigation");
     await pageList.waitForSelector("h1");
-  });
+  }, 9000);
 
-  test("The first link text 'Медиа Нетологии'", async () => {
-    time = 3000;
+  test("The link text 'Медиа Нетологии'", async () => {
     const actual = await page.$eval("header a + a", (link) => link.textContent);
     expect(actual).toContain("Медиа Нетологии");
-  });
+  }, 9000);
 
-  test("The first link leads on 'Медиа' page", async () => {
-    time = 4000;
+  test("The link leads on 'Медиа' page", async () => {
     await page.click("header a + a");
     await page.waitForSelector(".logo__media", {
       visible: true,
     });
     const actual = await page.$eval(".logo__media", (link) => link.textContent);
     expect(actual).toContain("Медиа");
-  });
+  }, 9000);
 });
 
-test("The first link text 'Учиться бесплатно'", async () => {
-  time = 6000;
+test("The link text 'Учиться бесплатно'", async () => {
+  page = await browser.newPage();
+  await page.goto("https://netology.ru");
   const firstLink = await page.$("header a + a");
   await firstLink.click();
   const pageList = await browser.newPage();
   await pageList.goto("https://netology.ru/free");
   await pageList.waitForSelector("h1");
-});
-test("The first link text 'Творческие профессии'", async () => {
-  time = 0;
+  page.close();
+}, 15000);
+test("The link text 'Творческие профессии'", async () => {
+  page = await browser.newPage();
+  await page.goto("https://netology.ru");
   const firstLink = await page.$("header a + a");
   await firstLink.click();
   const studyForFree = await browser.newPage();
   await studyForFree.goto("https://netology.ru/free/creative");
   await studyForFree.waitForSelector("h1");
-});
-test("The first link text 'Предложения от наших партнёров'", async () => {
-  time = 6000;
+  page.close();
+}, 9000);
+test("The link text 'Предложения от наших партнёров'", async () => {
+  page = await browser.newPage();
+  await page.goto("https://netology.ru");
   const firstLink = await page.$("header a + a");
   await firstLink.click();
   const studyForFree = await browser.newPage();
   await studyForFree.goto("https://netology.ru/partners-gifts");
   await studyForFree.waitForSelector("h1");
-});
+  page.close();
+}, 9000);
 test("The h1 should contain 'Работа'", async () => {
-  time = 4000;
+  page = await browser.newPage();
   const expected = "Работа";
   await page.goto("https://netology.ru/job");
   const actual = await page.$eval("h1", (link) => link.textContent);
   expect(actual).toContain(expected);
-});
+  page.close();
+}, 9000);
 test("The h1 should contain 'Отзывы'", async () => {
-  time = 5000;
+  page = await browser.newPage();
   const expected = "Отзывы студентов Нетологии";
   await page.goto("https://netology.ru/otzyvy");
   const actual = await page.$eval("h1", (link) => link.textContent);
   expect(actual).toContain(expected);
-});
+  page.close();
+}, 9000);
