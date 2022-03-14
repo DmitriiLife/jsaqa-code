a61fdefdb1719d07147cd1bc6d3ea3c757b503d1
mac <dmitri-shadri@yandex.ru>
2022-03-14 18:07:54 +0300

    changed test

--- 7.4/puppeteer/net.test.js
+++ 7.4/puppeteer/net.test.js
@@ -2,100 +2,106 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-});
+  await page.goto("https://netology.ru");
+}, 60000);
 
 afterEach(() => {
   page.close();
 });
 
+describe("easy test", () => {
+  test("Бесплатные курсы, лекции и полезные материалы", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/free"]');
+    await page.waitForSelector("h1");
+    const expected = "Бесплатные";
+    const actual = await page.$eval("h1", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 5000);
+
+  test("Творческие профессии", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/free"]');
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/free/creative"]');
+    await page.waitForSelector("h1");
+    const expected = "Творческие профессии";
+    const actual = await page.$eval(
+      'a[href$="/free/creative"',
+      (link) => link.textContent
+    );
+    expect(actual).toContain(expected);
+  }, 5000);
+
+  test("Новые профессии", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/new-courses"]');
+    await page.waitForSelector("h1");
+    const expected = "Новые профессии – курсы в Нетологии онлайн";
+    const actual = await page.$eval("title", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 5000);
+
+  test("Саморазвитие и хобби", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/lifestyle"]');
+    await page.waitForSelector("h1");
+    const expected = "Саморазвитие и хобби";
+    const actual = await page.$eval("h1", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 5000);
+
+  test("Программирование", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/development"]');
+    await page.waitForSelector("h1");
+    const expected = "Программирование";
+    const actual = await page.$eval("h1", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 5000);
+
+  test("Отзывы о курсах", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/otzyvy"]');
+    await page.waitForSelector("h1");
+    const expected = "Нетология – отзывы учеников о курсах";
+    const actual = await page.$eval("title", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 5000);
+});
+
 describe("Netology.ru tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://netology.ru");
-  });
-
-  afterEach(() => {
-    page.close();
-  });
-
-  test("The 'Медиа'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    await page.$("header a + a").click;
-    await page.goto("https://netology.ru/blog/");
+  test("Медиа", async () => {
+    await page.waitForSelector("h1");
+    await page.click("header a + a");
+    await page.waitForSelector("h1");
     const expected = "Медиа";
-    const actual  = await page.$eval("h1", (link) => link.textContent);
+    const actual = await page.$eval("h1", (link) => link.textContent);
     expect(actual).toContain(expected);
-  }, 9000);
+  }, 5000);
 
-  test("The 'Курсы'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    await page.$('a[href$="/navigation"]').click;
-    await page.goto("https://netology.ru/navigation");
-    const expected = "Курсы";
-    const actual  = await page.$eval("h1", (link) => link.textContent);
-    expect(actual).toContain(expected);
-  }, 9000);
-
-  test("The link text 'Медиа Нетологии'", async () => {
-    const actual = await page.$eval("header a + a", (link) => link.textContent);
-    expect(actual).toContain("Медиа Нетологии");
-  }, 9000);
-
-  test("The link leads on 'Медиа' page", async () => {
+  test("logo__media", async () => {
+    await page.waitForSelector("h1");
     await page.click("header a + a");
     await page.waitForSelector(".logo__media", {
       visible: true,
     });
     const actual = await page.$eval(".logo__media", (link) => link.textContent);
     expect(actual).toContain("Медиа");
-  }, 9000);
+  }, 5000);
+
+  test("Курсы", async () => {
+    await page.waitForSelector("h1");
+    await page.click('a[href$="/navigation"]');
+    await page.waitForSelector("h1");
+    const expected = "Курсы";
+    const actual = await page.$eval("h1", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 5000);
+
+  test("Медиа Нетологии", async () => {
+    await page.waitForSelector("h1");
+    const actual = await page.$eval("header a + a", (link) => link.textContent);
+    expect(actual).toContain("Медиа Нетологии");
+  }, 5000);
 });
-
-test("The link text 'Бесплатные курсы, лекции и полезные материалы'", async () => {
-  await page.goto("https://netology.ru");
-  await page.$('a[href$="/free"').click;
-  await page.goto("https://netology.ru/free");
-  const expected = "Бесплатные";
-  const actual  = await page.$eval("h1", (link) => link.textContent);
-  expect(actual).toContain(expected);
-}, 9000);
-
-test("The link text 'Творческие профессии'", async () => {
-  await page.goto("https://netology.ru");
-  await page.$('a[href$="/free"').click;
-  await page.goto("https://netology.ru/free");
-  await page.$('a[href$="/free/creative"').click;
-  await page.goto("https://netology.ru/free/creative");
-  const expected = "Творческие профессии";
-  const actual  = await page.$eval('a[href$="/free/creative"', (link) => link.textContent);
-  expect(actual).toContain(expected);
-}, 9000);
-
-test("The link text 'Предложения от наших партнёров'", async () => {
-  await page.goto("https://netology.ru");
-  await page.$('a[href$="/partners-gifts"').click;
-  await page.goto("https://netology.ru/partners-gifts");
-  const expected = "Предложения от наших партнёров";
-  const actual  = await page.$eval("h1", (link) => link.textContent);
-  expect(actual).toContain(expected);
-}, 9000);
-
-test("The h1 should contain 'Работа'", async () => {
-  await page.goto("https://netology.ru");
-  await page.$('a[href$="https://netology.ru/job"').click;
-  await page.goto("https://netology.ru/job");
-  const expected = "Работа";
-  const actual = await page.$eval("h1", (link) => link.textContent);
-  expect(actual).toContain(expected);
-}, 9000);
-
-test("The h1 should contain 'Отзывы'", async () => {
-  await page.goto("https://netology.ru");
-  await page.$('a[href$="/otzyvy"').click;
-  await page.goto("https://netology.ru/otzyvy");
-  const expected = "Отзывы студентов Нетологии";
-  const actual = await page.$eval("h1", (link) => link.textContent);
-  expect(actual).toContain(expected);
-}, 9000);
